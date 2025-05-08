34042cca761eacde81d50efb9b71739d2ea5a338
Harshal Jambhale <harshaljambhale01@gmail.com>
2025-05-08 15:20:54 +0530

    Delete ChatApp directory

--- ChatApp/ChatAppBackend/src/main/java/com/chatappbackend/repository/ChatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chatappbackend.repository;
-
-import com.chatappbackend.entities.Chat;
-import com.chatappbackend.entities.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatRepository extends MongoRepository<Chat, String> {
-
-}
--- ChatApp/ChatAppBackend/src/main/java/com/chatappbackend/socketConfig/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.chatappbackend.socketConfig;
-
-public class AppConstants {
-
-    public static final String FRONT_END_BASE_URL="http://localhost:5173";
-
-}
